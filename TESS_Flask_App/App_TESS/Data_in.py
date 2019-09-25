a222d5aad8348f3f312770ab7f1eafda9bb3ecfd
Michael Curry <mdcurry1138@gmail.com>
2019-09-25 14:08:44 -0400

    Going to Lunch
    created classes for each table, and functions to import each data table. Haven't testing ANYTHING.

--- TESS_Flask_App/App_TESS/Data_in.py
+++ TESS_Flask_App/App_TESS/Data_in.py
@@ -13,7 +13,7 @@ from .models import DB, Visual_Table
 
 
 # fetch TIC IDs from caltech
-def get_data():
+def get_visual_data():
     # Start by emptying the table (maybe make this more elegant later?)
     # Visual_Table.query.delete()
     # conn = sqlite.connect(db.sqlite3)
@@ -54,35 +54,76 @@ def get_data():
         raise e
 
     return dataproducts.to_sql(name='Visual_Table', con=DB.engine, index=False, 
-                               if_exists='append', )
-
-        # Move this df of tuples to sql. Also will need to move to postgress
-         #, index=False)
+                               if_exists='replace')
 
 
+def get_toi_data():
+    try: 
+        # Getting labelled TESS Objects of Interest dataframe from Caltech:
+        toi = pd.read_csv('https://exofop.ipac.caltech.edu/tess/' + 
+                    'download_toi.php?sort=toi&output=csv')
+    except:
+        print('failed to import initial csv from caltech')
+    else: 
+        try:
+            # Isolating columns we want:
+            toi = toi[['TIC ID',
+                'TOI',
+                'Epoch (BJD)',
+                'Period (days)',
+                'Duration (hours)',
+                'Depth (mmag)',
+                'Planet Radius (R_Earth)',
+                'Planet Insolation (Earth Flux)',
+                'Planet Equil Temp (K)',
+                'Planet SNR',
+                'Stellar Distance (pc)',
+                'Stellar log(g) (cm/s^2)',
+                'Stellar Radius (R_Sun)',
+                'TFOPWG Disposition',
+                ]]
+        except:
+            print('failed to filter df')
 
+    return toi.to_sql(name='TOI_Table', con=DB.engine, index=False, 
+                               if_exists='replace')
 
+def get_tic_catalog():
+    # Getting additional data on TESS Objects of Interest from STScI:
+    tic_catalog = pd.DataFrame()
+    for tic_id in tqdm(toi['TIC ID'].unique()):
+        row_data = Catalogs.query_criteria(catalog="Tic", ID=tic_id)
+        row_data = row_data.to_pandas()
+        tic_catalog = tic_catalog.append(row_data)
+    tic_catalog = tic_catalog.reset_index(drop=True)
 
+    # Renaming ID column to make this consistent with Caltech TOI dataframe:
+    tic_catalog = tic_catalog.rename(columns={'ID': 'TIC ID'})
 
+    # Isolating columns we want:
+    tic_catalog = tic_catalog[['TIC ID',
+                            'ra',
+                            'dec',
+                            'pmRA',
+                            'pmDEC',
+                            'plx',
+                            'gallong',
+                            'gallat',
+                            'eclong',
+                            'eclat',
+                            'Tmag',
+                            'Teff',
+                            'logg',
+                            'MH',
+                            'rad',
+                            'mass',
+                            'rho',
+                            'lum',
+                            'd',
+                            'ebv',
+                            'numcont',
+                            'contratio',
+                            'priority']]
 
-        # Took this out of the get_data function...
-        # useful_data = dataproducts[['target_name', 'target_name']]
-        # # Not sure if the below will work...
-        # for row in useful_data:
-        #     pair = Visual_Table(tic_id = useful_data['target_name']
-        #                         , data_url = useful_data['target_name'])
-        #     DB.session.add(pair)
-
-
-        # . . .
-        # dataproducts.to_sql(name='all_urls', con=conn, index=False)
-        # sqlite.commit()
-        # cur.close()
-        # conn.close()
-
-# eventually we'll need to move towards postgress... EXAMPLE
-# from sqlalchemy import create_engine
-# engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
-# df.to_sql('table_name', engine)
-
-# See: https://stackoverflow.com/questions/23103962/how-to-write-dataframe-to-postgres-table
+    return toi.to_sql(name='TIC_Cat_Table', con=DB.engine, index=False, 
+                               if_exists='replace')
